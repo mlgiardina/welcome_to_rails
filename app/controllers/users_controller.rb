01e73bf4194e207803e9c18d06dba794a11dc8cd
Matthew Giardina <mlgiardina@me.com>
2015-06-02 13:42:27 -0500

    can now see all users on generic GET .../users/

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+  def index
+    all_users = User.all
+    string_users = all_users.map do |user|
+      "(#{user.id}) #{user.first_name} #{user.last_name}, age: #{user.age}"
+    end
+    render text: string_users.join(" ")
+  end
+
+  def show
+    if User.exists?(params[:id])
+      user = User.find(params[:id])
+    render text: "(#{user.id}) #{user.first_name} #{user.last_name}, age: #{user.age}"
+    else
+      render text: "User Not Found", status: 404
+    end
+  end
+end
