1ce77d018b645c81d776130aa44d85232d9ac14e
Matthew Giardina <mlgiardina@me.com>
2015-06-02 15:44:04 -0500

    can now filter by first name and limit it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,40 @@
 class UsersController < ApplicationController
 
   def index
-    all_users = User.all
-    string_users = all_users.map do |user|
-      "(#{user.id}) #{user.first_name} #{user.last_name}, age: #{user.age}"
+    if params.include?("limit") && params.include?("offset")
+      limit_and_offset = User.all.limit(params[:limit]).offset(params[:offset])
+      show = limit_and_offset.map do |user|
+        "(#{user.id}) #{user.first_name} #{user.last_name}, age: #{user.age}"
+      end
+      render text: show.join("---")
+
+    elsif params.include?("limit")
+      limit = User.all.limit(params[:limit])
+      show = limit.map do |user|
+        "(#{user.id}) #{user.first_name} #{user.last_name}, age: #{user.age}"
+      end
+      render text: show.join(" ")
+
+    elsif params.include?("offset")
+      limit = User.all.offset(params[:offset])
+      show = limit.map do |user|
+        "(#{user.id}) #{user.first_name} #{user.last_name}, age: #{user.age}"
+      end
+      render text: show.join(" ")
+
+    elsif params.include?("first_name")
+      first_names = User.where("first_name LIKE ?", "#{params[:first_name]}%")
+      sorter = first_names.map do |user|
+        "(#{user.id}) #{user.first_name} #{user.last_name}, age: #{user.age}"
+      end
+      render text: sorter.join("-------------")
+    else
+      all_users = User.all
+      string_users = all_users.map do |user|
+        "(#{user.id}) #{user.first_name} #{user.last_name}, age: #{user.age}"
+      end
+      render text: string_users.join("---"), status: 200
     end
-    render text: string_users.join(" ")
   end
 
 
@@ -17,14 +46,5 @@ class UsersController < ApplicationController
       render text: "User Not Found", status: 404
     end
   end
-
-  def filter
-    first_names = User.where("first_name LIKE ?", "#{params[:first_name]}%")
-    sorter = first_names.map do |user|
-      "(#{user.id}) #{user.first_name} #{user.last_name}, age: #{user.age}"
-    end
-    render text: sorter.join("-------------")
-  end
-
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
 
-  get ':first_names', to: 'users#filter'
+  get '/users', to: 'users#index'
 
   get '/users/:id', to: 'users#show'
-
-  get '/users', to: 'users#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
