09fcb5da0f9f281f126606239676145bea7e3009
Matthew Giardina <mlgiardina@me.com>
2015-06-02 16:03:41 -0500

    replaced users with raker ones

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,49 +1,72 @@
 class UsersController < ApplicationController
 
   def index
+
     if params.include?("limit") && params.include?("offset")
-      limit_and_offset = User.all.limit(params[:limit]).offset(params[:offset])
-      show = limit_and_offset.map do |user|
-        "(#{user.id}) #{user.first_name} #{user.last_name}, age: #{user.age}"
-      end
-      render text: show.join("---")
-
+      limit_and_offset
     elsif params.include?("limit")
-      limit = User.all.limit(params[:limit])
-      show = limit.map do |user|
-        "(#{user.id}) #{user.first_name} #{user.last_name}, age: #{user.age}"
-      end
-      render text: show.join(" ")
-
+      limit
     elsif params.include?("offset")
-      limit = User.all.offset(params[:offset])
-      show = limit.map do |user|
-        "(#{user.id}) #{user.first_name} #{user.last_name}, age: #{user.age}"
-      end
-      render text: show.join(" ")
-
+      offset
     elsif params.include?("first_name")
-      first_names = User.where("first_name LIKE ?", "#{params[:first_name]}%")
-      sorter = first_names.map do |user|
-        "(#{user.id}) #{user.first_name} #{user.last_name}, age: #{user.age}"
-      end
-      render text: sorter.join("-------------")
+      first_name
+    elsif params.include?("delete")
+      delete_user
     else
-      all_users = User.all
-      string_users = all_users.map do |user|
-        "(#{user.id}) #{user.first_name} #{user.last_name}, age: #{user.age}"
-      end
-      render text: string_users.join("---"), status: 200
+      print_all_users
     end
   end
 
+  def delete_user
+    User.delete(params[:id])
+  end
+
+  def limit_and_offset
+    limit_and_offset = User.all.limit(params[:limit]).offset(params[:offset])
+    show = limit_and_offset.map do |user|
+      "(#{user.id}) #{user.first_name} #{user.last_name}, age: #{user.age}"
+    end
+    render text: show.join("---")
+  end
+
+  def print_all_users
+    all_users = User.all
+    string_users = all_users.map do |user|
+      "(#{user.id}) #{user.first_name} #{user.last_name}, age: #{user.age}"
+      end
+    render text: string_users.join("---"), status: 200
+  end
+
+  def first_name
+    first_names = User.where("first_name LIKE ?", "#{params[:first_name]}%")
+    sorter = first_names.map do |user|
+      "(#{user.id}) #{user.first_name} #{user.last_name}, age: #{user.age}"
+    end
+    render text: sorter.join("-------------")
+    end
+
+  def offset
+    limit = User.all.offset(params[:offset])
+    show = limit.map do |user|
+      "(#{user.id}) #{user.first_name} #{user.last_name}, age: #{user.age}"
+    end
+    render text: show.join(" ")
+  end
+
+  def limit
+    limits = User.all.limit(params[:limit])
+    show = limits.map do |user|
+      "(#{user.id}) #{user.first_name} #{user.last_name}, age: #{user.age}"
+    end
+    render text: show.join(" ")
+  end
 
   def show
     if User.exists?(params[:id])
       user = User.find(params[:id])
       render text: "(#{user.id}) #{user.first_name} #{user.last_name}, age: #{user.age}"
     else
-      render text: "User Not Found", status: 404
+    render text: "User Not Found", status: 404
     end
   end
 end
